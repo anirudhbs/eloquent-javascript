68a7b712cfd68ba5fd23ff38d25620bf4266cdd0
Anirudh B S <anirudhbs96@gmail.com>
2018-01-06 16:38:44 +0530

    Rename files

--- EloquentJavascript/practise.js
+++
@@ -1,6 +0,0 @@
-const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest,
-  req = new XMLHttpRequest();
-req.open("GET", './data.txt', false);
-req.send(null);
-console.log(req);
-// → This is the content of data.txt
